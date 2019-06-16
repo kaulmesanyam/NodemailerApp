0394357e64d41f2a87bfb06d18a65b57ac231d12
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-16 16:32:38 +0530

    Added public static folder path

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
-
+const path = require('path');
 const app = express();
 
 //View engine setup
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
