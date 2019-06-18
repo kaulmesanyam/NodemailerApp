9de5a640f0145f54b8d234aed306fdc4a8b61915
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-18 17:52:42 +0530

    Added nodemailer code to the app.js file
    The source email request is not updated as I don't have an SMTP
    server at the moment.

--- app.js
+++ app.js
@@ -34,7 +34,41 @@ app.post('/send', (req, res) => {
 		<p>${req.body.message}</p>
 	`;
   	
+  	 // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    },
+    tls:{
+    	rejectUnauthorized:false
+    }
+  });
 
+  let mailOptions = {
+  	from: '"Sanyam Kaul Nodemailer App" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Nodemailer contact request", // Subject line
+    text: "Nodemailer contact request", // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+  	if(error) {
+  		return console.log(error);
+  	} 
+  	console.log("Message sent: %s", info.messageId);
+  	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  	res.render('contact', {msg:'Email has been sent'});
+  });
 });
 
 app.listen(3000, () => console.log('Server started...'));
+
+
+
